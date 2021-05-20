520661f4337cb331df818147576d03e0892dec24
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-20 06:37:13 +0530

    login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Button, Center } from '@chakra-ui/react';
 
 export default function Login() {
+
+    const login = () => {
+
+    }
+
     return (
         <div>
-
+            <Center h="80vh">
+                <Button onClick={login} size="lg" style={{ fontSize: "20px", padding: "30px 40px" }}>
+                    Login With Google <img style={{ marginLeft: "5px" }} src="/images/google.svg" alt="" />
+                </Button>
+            </Center>
         </div>
     )
 }
