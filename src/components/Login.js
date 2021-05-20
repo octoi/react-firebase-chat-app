b8746896a70d7bd37bdee662b527abf55a348bfa
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-20 06:57:49 +0530

    sign out and loading

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { auth, provider } from '../firebase/firebase';
 import { Button, Center } from '@chakra-ui/react';
 
-export default function Login() {
+export default function Login({ loading }) {
 
-    const login = () => {
-
+    const signInWithGoogle = () => {
+        auth.signInWithPopup(provider)
     }
 
     return (
         <div>
             <Center h="80vh">
-                <Button onClick={login} size="lg" style={{ fontSize: "20px", padding: "30px 40px" }}>
+                <Button disabled={loading} onClick={signInWithGoogle} size="lg" style={{ fontSize: "20px", padding: "30px 40px" }}>
                     Login With Google <img style={{ marginLeft: "5px" }} src="/images/google.svg" alt="" />
                 </Button>
             </Center>
