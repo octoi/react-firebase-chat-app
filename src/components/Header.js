14f84107b4b4c1dc7a5e38e15dac399c97a53985
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-20 06:56:28 +0530

    sign out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { auth } from '../firebase/firebase';
 import { Button, Text } from '@chakra-ui/react';
 
 export default function Header({ user }) {
+
+    const signOut = () => {
+        auth.signOut();
+    }
+
     return (
         <div style={{ display: "flex", justifyContent: "space-between" }}>
             <Text style={{ display: "flex" }}>
                 <img width="40px" src="/images/react.svg" alt="react" />
                 <img width="40px" src="/images/firebase.svg" alt="firebase" />
             </Text>
-            <Button size="lg" disabled={user ? false : true}>{user ? "Sign Out" : "Login Nerd"}</Button>
+            <Button size="lg" onClick={signOut} disabled={user ? false : true}>{user ? "Sign Out" : "Login Nerd"}</Button>
         </div>
     )
 }
