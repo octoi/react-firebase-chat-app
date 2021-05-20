c70555f9de67eec6be7504e06584e87c04d6ac89
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-20 06:32:33 +0530

    component routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase/firebase';
 import ChakraWrap from './utils/ChakraWrap';
 import Header from './components/Header';
+import Login from './components/Login';
+import Chat from './components/Chat';
 import './App.css'
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
     return (
         <ChakraWrap>
             <Header user={user} />
+            {user ? <Chat /> : <Login />}
         </ChakraWrap>
     );
 }
