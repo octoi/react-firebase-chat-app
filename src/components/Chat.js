0817587c781f13a507f0d1e5f738f250a21646eb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-20 12:17:30 +0530

    chat functionality

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,14 @@
-import { useState } from 'react';
-import { auth } from '../firebase/firebase';
+import firebase from 'firebase/app';
+import { useState, useRef, useEffect } from 'react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { auth, firestore } from '../firebase/firebase';
 import { Input, Flex, Button, Avatar } from '@chakra-ui/react';
 
 
 export default function Chat() {
     const [formValue, setFormValue] = useState("");
+    const messagedRef = firestore.collection('messages');
+    const dummy = useRef();
 
     const user = {
         name: auth?.currentUser.displayName,
@@ -12,14 +16,22 @@ export default function Chat() {
         profile: auth?.currentUser.photoURL,
     }
 
-    const sendMessage = () => {
+    const sendMessage = async (e) => {
+        e.preventDefault();
 
+        await messagedRef.add({
+            message: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            user,
+        });
+
+        dummy.current.scrollIntoView({ behavior: 'smooth' });
+        setFormValue("")
     }
 
     return (
         <Flex direction="column" justifyContent="space-between" marginTop="20px">
-            <div style={{ height: "75vh", overflowX: "hidden", margin: "20px 0px" }}>
-            </div>
+            <Messages messagedRef={messagedRef} user={user} dummy={dummy} />
             <form onSubmit={sendMessage} style={{ display: "flex" }}>
                 <Input
                     variant="filled"
@@ -34,13 +46,31 @@ export default function Chat() {
     )
 }
 
-function ChatMessage({ user, chat }) {
+function Messages({ messagedRef, user, dummy }) {
 
-    const isMyMessage = user.email === chat.sender.email
+    const query = messagedRef.orderBy('createdAt').limit(25);
+
+    const [messages] = useCollectionData(query, { idField: 'id' });
+
+    useEffect(() => {
+        dummy.current.scrollIntoView({ behavior: 'smooth' });
+    }, [messages])
+
+    return (
+        <div style={{ height: "75vh", overflowX: "hidden", margin: "20px 0px" }}>
+            {messages && messages.map((message, id) => <ChatMessage user={user} chat={message} key={id} />)}
+            <span ref={dummy}></span>
+        </div>
+    );
+}
+
+function ChatMessage({ chat, user }) {
+
+    const isMyMessage = user.email === chat.user.email
 
     return (
         <Flex alignItems="center" flexDirection={isMyMessage ? "row-reverse" : "row"} marginTop="15px">
-            <Avatar src={chat.sender.profile} />
+            <Avatar src={chat.user.profile} />
             <div style={{
                 margin: "0px 10px",
                 background: "var(--chakra-colors-whiteAlpha-200)",
